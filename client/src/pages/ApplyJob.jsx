478d12742839157dec06d66b61b85afdce7b2073
Nadimul Haque <nadimulhaque05@gmail.com>
2025-01-03 00:43:22 +0530

    Completed till Job Listing Page

---
+++ client/src/pages/ApplyJob.jsx
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+
+const ApplyJob = () => {
+
+  const { id } = useParams()
+  const [jobData, setJobData] = useState(null)
+
+  const { jobs } = useContext(AppContext)
+
+  const fetchJob = async () => {
+    const data = jobs.filter(job => job._id === id)
+    if (data.length !== 0) {
+      setJobData(data[0])
+      console.log(data[0])
+    }
+  }
+
+
+  useEffect(()=> {
+    fetchJob()
+  },[id])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ApplyJob
